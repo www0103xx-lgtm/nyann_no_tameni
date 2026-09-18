8764f948fe5ba83b7d2b52b4684fd482fc90d6ef
Your Name <you@example.com>
2026-09-18 22:08:27 +0900

    14-体重記録編集機能

--- spec/system/weight_records_spec.rb
+++ spec/system/weight_records_spec.rb
@@ -114,4 +114,56 @@ RSpec.describe "体重記録", type: :system do
     expect(page).to have_content("体重を入力してください")
     expect(diet_challenge.weight_records.count).to eq(0)
   end
+
+  it "不正な体重の場合は更新できない" do
+    weight_record = create(
+      :weight_record,
+      diet_challenge: diet_challenge,
+      weight: 60.0,
+      recorded_on: Date.current
+    )
+
+    visit new_user_session_path
+
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: user.password
+    click_button "ログイン"
+
+    expect(page).to have_current_path(dashboard_path)
+
+    visit edit_weight_record_path(weight_record)
+
+    fill_in "今日の体重", with: ""
+    click_button "更新する"
+
+    expect(page).to have_content("体重を入力してください")
+
+    weight_record.reload
+
+    expect(weight_record.weight).to eq(60.0)
+  end
+
+  it "他のユーザーの体重記録は編集できない" do
+    other_user = create(:user)
+    other_diet_challenge = create(:diet_challenge, user: other_user)
+    other_weight_record = create(
+      :weight_record,
+      diet_challenge: other_diet_challenge,
+      weight: 55.0,
+      recorded_on: Date.current
+    )
+
+    visit new_user_session_path
+
+    fill_in "メールアドレス", with: user.email
+    fill_in "パスワード", with: user.password
+    click_button "ログイン"
+
+    expect(page).to have_current_path(dashboard_path)
+
+    visit edit_weight_record_path(other_weight_record)
+
+    expect(page).to have_content("ActiveRecord::RecordNotFound")
+    expect(page).not_to have_content("今日の体重を編集")
+  end
 end
